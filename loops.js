3be5ceff05351c0bb31303ffe3a99d4612ac7e06
Sushant Ganeshan <sgbharat.joint@gmail.com>
2022-04-26 19:00:52 +0530

    Initial Commit

---
+++ loops.js
@@ -0,0 +1,8 @@
+for(var a=1; a<=5; a++){
+    for(var b=1; b<=a; b++){
+        document.write(" "+b); //inner loop to print value of b
+    }
+    document.write("<br>"); //for next line break outside inner loop
+}
+document.body.style.fontFamily="verdana";
+document.body.style.fontSize="50px";
